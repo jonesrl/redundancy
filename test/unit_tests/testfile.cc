18462a04cab3cb3ea5671bb0624be66167c1cb0a
Randall Jones <randall.jones5@gmail.com>
2012-03-14 21:58:20 -0500

    disabled warning for the Catch unit test framework
    I increased the warning levels substantially for the debug build, and
    causing g++ to flag 500+ warnings when scanning the Catch unit test
    framework. I disabled all warnings the the unit test framework using GCC
    pragmas.

--- test/unit_tests/testfile.cc
+++ test/unit_tests/testfile.cc
@@ -1,3 +1,8 @@
+// avoid tons of warnings that I can't fix
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic warning "-w"
+#pragma GCC diagnostic pop
 #include "catch.hpp"
-
+#endif
 
--- test/unit_tests/unittestsrunner.cc
+++ test/unit_tests/unittestsrunner.cc
@@ -1,2 +1,9 @@
 #define CATCH_CONFIG_MAIN
+// avoid tons of warnings that I can't fix
+#ifdef __GNUC__
+//#pragma GCC system_header
+#pragma GCC diagnostic push
+#pragma GCC diagnostic warning "-w"
+#pragma GCC diagnostic pop
 #include "catch.hpp"
+#endif
